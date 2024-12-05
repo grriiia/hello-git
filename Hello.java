874a3c32b3d6ccc325ffdd54c754e0285af5819b
grriiia <lkh302011@naver.com>
2024-12-05 06:45:53 +0000

    C2

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello world GIT!");
+		System.out.println("Changes after first commit!");
+        System.out.println("2");
 	}
 }
